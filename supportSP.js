2eaa45207fb31f3054f2f9f2d57c695790888a96
wmfrpafj <111269771+wmfrpafj@users.noreply.github.com>
2023-01-17 19:00:35 +0900

    Create supportSP.js

---
+++ supportSP.js
@@ -0,0 +1,7 @@
+var summon = prompt("현재 뽑스피(소환SP)를 입력해주세요","메세지를 입력해주세요.")
+var have = prompt("현재 보유 SP를 입력해주세요","메세지를 입력해주세요.")
+var wave = prompt("현재 웨이브를 입력해주세요","메세지를 입력해주세요.")
+
+var finalS = ((summon/10)+1)*(summon/20)
+var finalH = have - ((wave-45)/4)*1050 - ((wave-45)/2)*1000 - 18900 - 3500
+var SP = Math.ceil(finalS*10 + finalH)
