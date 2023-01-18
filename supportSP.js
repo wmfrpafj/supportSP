5f5eb09b267c1ead85718478896fc00ed0af4f01
wmfrpafj <111269771+wmfrpafj@users.noreply.github.com>
2023-01-18 14:47:02 +0900

    Update supportSP.js

--- supportSP.js
+++ supportSP.js
@@ -5,3 +5,4 @@ var wave = prompt("현재 웨이브를 입력해주세요","메세지를 입력�
 var finalS = ((summon/10)+1)*(summon/20)
 var finalH = have - ((wave-45)/4)*1050 - ((wave-45)/2)*1000 - 18900 - 3500
 var SP = Math.ceil(finalS*10 + finalH)
+var deal = Math.ceil(SP + ((wave-45)/4)*1050 + ((wave-45)/2)*1000 + 18900 + 3500)
